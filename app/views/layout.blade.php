c8d1b50593a53991902bc17ae495742588f1d9bf
skimberk1 <skimberk1@gmail.com>
2014-10-23 06:30:10 -0400

    Added basic styles.

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -3,8 +3,12 @@
 <head>
 	<meta charset="UTF-8">
 	<title>DWA Project 3</title>
+
+	<link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
-	@yield('content')
+	<div class="container">
+		@yield('content')
+	</div>
 </body>
 </html>
